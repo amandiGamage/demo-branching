bb92120545bc43f9a51eeb60fa64409b701d4bb6
Amandi Imasha <Amandiimasha43@gmail.com>
2018-09-02 21:37:58 +0530

    write blog

--- src/main/java/com/ijse/demo/branching/DemoBranchingApplication.java
+++ src/main/java/com/ijse/demo/branching/DemoBranchingApplication.java
@@ -8,5 +8,27 @@ public class DemoBranchingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBranchingApplication.class, args);
+		String s= "\n" +
+				"\n" +
+				"\n" +
+				"\n" +
+				"\n" +
+				"dan oya inne feature branch eke dan oya hithanna oyata thawa feature branch ekak hadanna one kiyala\n" +
+				"dan oya methana idan heduwoth feature/-confug eka base thamai feature branch eka hedenne ehema karanne ne\n" +
+				"nitharama feature branch ekak hedenna one develop kiyana branch eka base eka nisa api mulin develop ekata switch wenna one\n" +
+				"\n" +
+				"dan oya yanawanam feature branch ekak hadanna dan hadenne develop eka base\n" +
+				"\n" +
+				"me feature ekak oya develop karala iwara unama eka pull request ekak danna one development branch ekata.\n" +
+				"git eke master/develop kiyana branch deka witharai anthimatama ithuru wenne , me feature branch use krnne feture eka develop krnna\n" +
+				"bug fix branch kadaganne bug ekak fix karanna, hot fix kadaganne production code eke bug ekak thibboth eka ikmananin hadanna hadanne\n" +
+				"me nam walin kadagaththa branch wala idan ethakota risk ekak ne kiyala therenawa neda oyage athin code ekata kela unath\n" +
+				"e oyage branch eke idan witharai oyata puluwan step ekak back wenna oyage branch eka delete karala dala development eken apahu kadagannawa\n" +
+				"code ekata risk ekak ne kotinma therenawa neda production code eka safe ekai develop master kiyana dekata marged karanna\n" +
+				"responsibility eka thiyena kenekta witharak access denne dev team ekata puluwan code eke clone ekak gannai feature branch hadagannai\n" +
+				"ewata code eka push karannai witharai ewa repo settings walin hadanne mn ekath pennannm\n" +
+				"\n" +
+				"\n" +
+				"dan oya inne oyage aluth feature branch eke dan code eka push karanna yanawa kiyala hithanna";
 	}
 }
