76dc07ba9cb1506f69963a384bbb11d59cb49699
Amandi Imasha <Amandiimasha43@gmail.com>
2018-09-02 21:21:34 +0530

    initial commit

---
+++ src/test/java/com/ijse/demo/branching/DemoBranchingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ijse.demo.branching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoBranchingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
